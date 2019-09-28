410f424db510a864dd1c1618128fe69b5a52d910
Sebastian <smarucci@avioconsulting.com>
2019-09-28 19:21:22 -0300

    Refactor reddit reducer to include status field to indicate the status of the entries (NOT_LOADED, LOADED, LOADING, ERROR_LOADING)

--- src/app/reddit/reddit.js
+++ src/app/reddit/reddit.js
@@ -1,21 +1,30 @@
 import get from 'lodash/get';
 import size from 'lodash/size';
 
-export const redditEntryReducer = (state = [], action) => {
+export const redditEntryReducer = (state = {status: "NOT_LOADED", data: []}, action) => {
   switch (action.type) {
+    case 'LOAD_REDDIT_ENTRIES_PENDING':
+      return {...state, status: "LOADING"};
+
+    case 'LOAD_REDDIT_ENTRIES_ERROR':
+      return {...state, status: "ERROR_LOADING"};
+
     case 'LOAD_REDDIT_ENTRIES_SUCCESS':
-      if(size(get(action.payload, "data.children")) > 0){
-        return action.payload.data.children.map(c => c.data);
+      let result = {status: "LOADED", data: []};
+      if (size(get(action.payload, "data.children")) > 0) {
+        result.data = action.payload.data.children.map(c => c.data);
       }
-      return [];
+      return result;
 
     case 'UPDATE_REDDIT_ENTRY_SUCCESS':
-      return state.map(e => {
-        if (e.id === action.payload.id){
-          return action.payload;
-        }
-        return e;
-      });
+      return {
+        status: state.status, data: state.data.map(e => {
+          if (e.id === action.payload.id) {
+            return action.payload;
+          }
+          return e;
+        })
+      };
 
     default:
       return state;
--- src/app/reddit/selector.js
+++ src/app/reddit/selector.js
@@ -1,7 +1,11 @@
 export const getTop50Entries = (state) => {
-  return state.redditEntry.slice(0,50);
+  return state.redditEntry.data.slice(0,50);
 };
 
 export const getRedditEntry = (state, entryId) => {
-  return state.redditEntry.find(e => e.id === entryId);
+  return state.redditEntry.data.find(e => e.id === entryId);
 };
+
+export const getRedditLoadStatus = state => {
+  return state.redditEntry.status;
+}
