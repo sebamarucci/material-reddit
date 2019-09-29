1d109b84c5632a5c1725ab65b9155564910acbc8
Sebastian <smarucci@avioconsulting.com>
2019-09-29 17:45:10 -0300

    Code cleanup

--- src/App.js
+++ src/App.js
@@ -29,18 +29,4 @@ export default function App(props) {
     </Provider>
   );
 }
-// class App extends Component {
-//   render() {
-//     return <div className="App">
-//       <div className="App-heading App-flex">
-//         <h2>Welcome to <span className="App-react">React</span></h2>
-//       </div>
-//       <div className="App-instructions App-flex">
-//         <img className="App-logo" src={require('./react.svg')}/>
-//         <p>Edit <code>src/App.js</code> and save to hot reload your changes.</p>
-//       </div>
-//     </div>
-//   }
-// }
-//
-// export default App
+
